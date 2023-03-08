332ed7a122070ccba8ce17f8c15d913756a873eb
Daiki Nakane <nakane2501@gmail.com>
2023-03-08 16:49:58 +0900

    fix a listener node

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,12 +1,12 @@
-1 import rclpy
-2 from rclpy.node import Node
-3 from std_msgs.msg import Int16
-4
-5 def cb(msg):
-6     global node
-7     node.get_logger().info("Listen: %d" % msg.data)
-8
-9 rclpy.init()
-10 node = Node("listener")
-11 pub = node.create_subscription(Int16, "countup", cb, 10)
-12 rclpy.spin(node)  
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
+rclpy.init()
+node = Node("listener")
+sub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)  
